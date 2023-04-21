864afe6b47af4ffcb3a7ab79ce16aa6da6d07e85
Hasan <emin.oren.139@gmail.com>
2023-04-21 17:22:06 +0300

    Change main

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,16 @@
 // Copyright 2021 GHA Test Team
 #include <textgen.h>
 #include <fstream>
+#include <iostream>
 
 
 int main()
 {
     setlocale(LC_ALL, "");
-    std::wstring text;
-    std::wstring line;
-
-    std::wifstream in("fish.txt");
-    if (in.is_open())
-    {
-        while (getline(in, line))
-        {
-            text += line + L"\n";
-        }
-    }
-    in.close();
+    std::wstring text = L"You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. You’re on your own.";
 
     Markov gen = Markov(text, 2);
 
     std::wofstream out;
-    out.open("fish_generate.txt");
-    if (out.is_open())
-    {
-        out << gen.Generate(3000) << std::endl;
-    }
-    out.close();
+    std::wcout << gen.Generate(500);
 }
